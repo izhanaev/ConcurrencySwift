9b0a4dc17efee5a25cfa485447e43b16b74216ea
Ilyas Zhanaev <izhanaev@mail.ru>
2024-08-04 18:09:48 +0300

    Initial Commit

---
+++ ConcurrencySwift/ConcurrencySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencySwiftApp.swift
+//  ConcurrencySwift
+//
+//  Created by Ильяс Жанаев on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
